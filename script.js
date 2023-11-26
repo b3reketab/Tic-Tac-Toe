3d2a3ee62600c9e9816fc82eb3bf94bf87010ecc
b3reketab <141371508+b3reketab@users.noreply.github.com>
2023-11-27 01:07:56 +0300

    modify check for win

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const plOne = document.querySelector('#name1')
 const plTwo = document.querySelector('#name2')
 const strtBtn = document.querySelector('.start')
-const resBtn = document.querySelector('#res')
 
 function Gameboard() {
     const board = []
@@ -11,32 +10,27 @@ function Gameboard() {
             board[i].push(Cell())
         }
     }
-
     const getBoard = () => board
-
     const setValue = (row, column, token) => {
         if(board[row][column].getValue() === '') {
             board[row][column].addToken(token)
             return true
         }
     }
-
     return { getBoard, setValue }
 }
+
 function Cell() {
     let value = ''
-
     const addToken = (token) => {
         value = token
     }
-
     const getValue = () => value
-
     return { getValue, addToken }
 }
+
 function GameController(a, b) {
     const board = Gameboard()
-
     const player = [{
             name: a || 'X',
             token: 'X',
@@ -44,36 +38,47 @@ function GameController(a, b) {
             name: b || 'O',
             token: 'O',
         }]
-
     let activePlayer = player[0]
-
     const getActive = () => activePlayer
-
     const switchPlayer = () => getActive() === player[0] 
                                     ? activePlayer = player[1] 
-                                    : activePlayer = player[0]
-
+                                    : activePlayer = player[0]   
     const playRound = (row, column) => {
         const checker = board.setValue(row, column, getActive().token)
+        const valBoard = board.getBoard().map(row => row.map(cell => cell.getValue()))
+        let diagcol = []
+        diagcol[3] = []
+        diagcol[4] = []
+        for(let i = 0; i < 3; i++) {
+            diagcol[i] = []
+            for(let j = 2; j >= 0; j--) {
+                diagcol[i].push(valBoard[j][i])
+                if(i === j) diagcol[3].push(valBoard[i][j])
+                diagcol[4].push(valBoard[i][j])
+            }  
+        }
+        const winFill = valBoard.concat(diagcol).filter(row => row.every(cell => cell === 'X') || row.every(cell => cell === 'O'))
+        if(winFill.length) {
+            return `${getActive().name} wins!!`
+        } else if(!valBoard.filter(row => row.includes('')).length) {
+            return `It's a tie!`
+        }
         if(checker) switchPlayer()   
     }
-
-    return { playRound, getActive, getBoard: board.getBoard, switchPlayer }
+    return { playRound, getActive, getBoard: board.getBoard }
 }
 
 function ScreenController() {
     const game = GameController(plOne.value, plTwo.value)
     const playerTurn = document.querySelector('.turn')
     const boarDiv = document.querySelector('.board')
-    const msgDiv = document.querySelector('.msg')
-
+    const result = document.querySelector('.msg')
+    let winner = ''
     const updateScreen = () => {
-        boarDiv.textContent = ''
-
         const board = game.getBoard()
         const activePlayer = game.getActive()
+        boarDiv.textContent = ''
         playerTurn.textContent = `${activePlayer.name}'s turn...`
-
         board.forEach((row, index) => {
             row.forEach((cell, indx) => {
                 const cellBtn = document.createElement('button')
@@ -83,56 +88,26 @@ function ScreenController() {
                 cellBtn.textContent = cell.getValue()
                 boarDiv.appendChild(cellBtn)
             })
-        });
-        const valBoard = board.map(row => row.map(cell => cell.getValue()))
-
-        const column = []
-        for(let i = 0; i < 3; i++) {
-            column[i] = []
-            for(let j =0; j < 3; j++)
-            column[i].push(valBoard[j][i])            
-        }
-
-        let i = 2
-        const diagonal = []
-        diagonal.push(valBoard.map((row, i) => row[i]))
-        diagonal.push(valBoard.map(row => row[i--]))
-
-        const winCol = column.filter(row => row.every(cell => cell === 'X') || row.every(cell => cell === 'O'))
-
-        const winDiag = diagonal.filter(row => row.every(cell => cell === 'X') || row.every(cell => cell === 'O'))
-
-        const winRow = valBoard.filter(row => row.every(cell => cell === 'X') || row.every(cell => cell === 'O'))
-
-        const tiendicator = valBoard.filter(row => row.every(cell => cell !== ''))
-
-        if(winCol.length || winRow.length || winDiag.length) {
-            game.switchPlayer()
-            const winner = game.getActive()
-            msgDiv.textContent = `${winner.name} won!!!`
-            playerTurn.textContent = ''
-        } else if(tiendicator.length === 3) {
-            msgDiv.textContent = 'Its a tie!!!';
-            playerTurn.textContent = ''
-        }
+        })
+        boarDiv.addEventListener('click', (e) => {
+            if(winner) {
+                playerTurn.textContent = ''
+                result.textContent = winner
+            } else {
+                clickHandler(e)
+            }
+        })
     }
-    function clickHandler(e) {
-        if(msgDiv.textContent) return
-
+    const clickHandler = (e) => {
         const selectedCol = e.target.dataset.column
         const selectedRow = e.target.dataset.row
-
-        game.playRound(selectedRow, selectedCol)
+        winner = game.playRound(selectedRow, selectedCol)
         updateScreen()
     }
-
-    boarDiv.addEventListener('click', clickHandler)
     updateScreen()
 }
-
 let i = 0
-
 strtBtn.addEventListener('click', () => {
-    i == 0 ? ScreenController() : document.location.reload()
+    i === 0 ? ScreenController() : document.location.reload()
     i++
 })
